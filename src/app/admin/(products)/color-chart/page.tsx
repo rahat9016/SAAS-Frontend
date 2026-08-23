d55e0284778f7387e32b066d766986082f215220
rahat9016 <minhajurrohoman9016@gmail.com>
2026-08-23 10:51:56 +0600

    refactor: Remove Color and Size Chart components and related schemas

--- src/app/admin/(products)/color-chart/page.tsx
+++
@@ -1,9 +0,0 @@
-import ColorChartList from "@/src/components/admin/Categories/ColorChartList/ColorChartList";
-
-export default function page() {
-  return (
-    <div>
-      <ColorChartList />
-    </div>
-  );
-}
--- src/app/admin/(products)/size-chart/page.tsx
+++
@@ -1,9 +0,0 @@
-import SizeChartList from "@/src/components/admin/Categories/SizeChartList/SizeChartList";
-
-export default function page() {
-  return (
-    <div>
-      <SizeChartList />
-    </div>
-  );
-}
